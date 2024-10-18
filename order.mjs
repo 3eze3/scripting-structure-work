d6bd5376b1732f0f417e47f7d60e784b169027cc
3eze3 <ezekics33@gmail.com>
2024-10-17 22:53:00 -0500

    main function

--- order.mjs
+++ order.mjs
@@ -1,56 +1,17 @@
 #!/usr/bin/env node
-import { argv, exit } from "node:process"
-import { structureBase } from "./utils/base.mjs"
-const carpetas = {
-	// "./": [".gitignore", "README.md"],
-	build: [
-		"css",
-		"html",
-		"html/index.html",
-		"css/style.css",
-		"assest",
-		"assest/fonts",
-		"assest/images",
-		"assest/images/icons",
-		"assest/images/ilustration",
-	],
-	src: [
-		"base",
-		"abstract",
-		"layout",
-		"components",
-		"style.scss",
-		"base/_reset.scss",
-		"base/_fonts.scss",
-		"abstract/_minxins.scss",
-		"abstract/_variables.scss",
-	],
-}
+import { argv } from "node:process"
+import { help } from "./utils/help.mjs"
+import { aprendizaje } from "./structure/aprendizaje.mjs"
+import { vanilla } from "./structure/vanilla.mjs"
 
 function menu() {
-	const parameter = argv[2]
-	if (!parameter) {
-		console.log(
-			"\n[+] Estructura para un projecto base [Html, Css/Scss , Js]"
-		)
-		structureBase()
-	}
-	// if (parameter.lowerCase() == "fm") {
-	// 	console.log("\n[+] Estructura para proyecto Frontend Mentor\n")
-	// } else if (parameter.lowerCase() == "van") {
-	// 	console.log(
-	// 		"\n[+] Estructura para proyecto Vanilla [Html, Css/Scss, JS/TS]\n"
-	// 	)
-	// } else if (parameter.lowerCase() == "react") {
-	// 	console.log(
-	// 		// Debo revisar como es la estructura de projectos en react
-	// 		"\n[+] Estructura para proyecto de React \n"
-	// 	)
-	// } else if (parameter.lowerCase() == "inte") {
-	// 	// Establecer bien el flujo de carpetas generales
-	// 	console.log(
-	// 		"\n[+] Estructura para proyecto Integrador [Hacking , Frontend ]\n"
-	// 	)
-	// }
+	const option = argv[2]
+	if (!option) help()
+	if (option === "-a") aprendizaje()
+	else if (option === "-v") vanilla()
+	else if (option === "-r") console.log("React, en construction...")
+	else if (option === "-i") console.log("Integrador en construction..")
+	else if (option === "-c") console.log("Contenido en construction...")
+	else help()
 }
 menu()
