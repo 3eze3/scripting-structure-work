67fb044e2b17a4976cbce34de92883251328f1c6
3eze3 <ezekics33@gmail.com>
2024-10-21 11:48:47 -0500

    Add the configuration for each project

--- order.mjs
+++ order.mjs
@@ -3,15 +3,20 @@ import { argv } from "node:process"
 import { help } from "./utils/help.mjs"
 import { aprendizaje } from "./structure/aprendizaje.mjs"
 import { vanilla } from "./structure/vanilla.mjs"
+import { videos } from "./structure/videos.mjs"
+import { node } from "./structure/node.mjs"
+import { react } from "./structure/react.mjs"
+import { integrador } from "./structure/integrador.mjs"
 
 function menu() {
 	const option = argv[2]
 	if (!option) help()
 	if (option === "-a") aprendizaje()
 	else if (option === "-v") vanilla()
-	else if (option === "-r") console.log("React, en construction...")
-	else if (option === "-i") console.log("Integrador en construction..")
-	else if (option === "-c") console.log("Contenido en construction...")
+	else if (option === "-n") node()
+	else if (option === "-r") react()
+	else if (option === "-i") integrador()
+	else if (option === "-c") videos()
 	else help()
 }
 menu()
