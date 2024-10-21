8889f12e0132818888c41a8701c70babc6f1a697
3eze3 <ezekics33@gmail.com>
2024-10-21 12:01:29 -0500

    Rename file

---
+++ rutas/rutas.mjs
@@ -0,0 +1,14 @@
+import { join } from "node:path"
+const styles = "./Templates/resetStyles/scss"
+const publicaciones = "./Templates/publicaciones"
+const config = "./Templates/tsconfig"
+
+export const templateFiles = [
+	join(styles, "/base/_reset.scss"),
+	join(styles, "/base/_fonts.scss"),
+	join(styles, "/abstract/_mixins.scss"),
+	join(styles, "/abstract/_variables.scss"),
+	join(publicaciones, "README.md"),
+	join(publicaciones, "linkedin.md"),
+	join(config, "individual/tsconfig.json"),
+]
